8ec5329649a5b0b175a1862b16c43253a4b5c854
OtavioOLDG <otaviolg2000@gmail.com>
2026-03-27 21:59:11 -0300

    criei o footer e o header e content inicial

--- my-app/server.js
+++ my-app/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
+const morgan = require('morgan');
+const helmet = require('helmet');
+const { title } = require('process');
+
 require('dotenv').config();
 
 const PORT = process.env.PORT || 2000;
 
+//Adicionei o helmet e o morgan (São adicionais)
+app.use(helmet())
+app.use(morgan('dev'))
+
+// Aqui eu to configurando a engine 
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 
@@ -16,12 +27,12 @@ app.listen(PORT, () => {
 // Rota principal
 app.get('/', (req, res) => {
     // res.render manda o EJS desenhar o arquivo 'index.ejs'
-    res.render('index', { mensagem: 'O EJS está funcionando perfeitamente!' });
+    res.render('index', { title: 'O EJS está funcionando perfeitamente!' });
 });
 
 app.get('/test', (req, res) => {
     res.render('index', { 
         mensagem: 'O EJS está funcionando!',
-        whatsapp: process.env.WHATSAPP_NUMERO 
+        whatsapp: process.env.WHATSAPP_MENSAGEM
     });
 });
